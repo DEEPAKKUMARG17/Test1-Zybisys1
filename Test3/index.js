e9fc5c56f7d67948d0180b9b8340c9b64aab0f4b
DEEPAKKUMARG17 <cse221022@saranathan.ac.in>
2023-08-18 17:43:54 +0530

    done the program

--- Test3/index.js
+++ Test3/index.js
@@ -12,6 +12,7 @@ const client=new MongoClient(url);
 app.use(cookieParser());
 
 //Q.4 user defined events to write in a file
+/* 4. Create a user defined event in node which when fired should write some content to a file. */
 app.get('/', (req, res) => {
   
  const eventEmitter=new events.EventEmitter()
@@ -44,6 +45,8 @@ let user1={
     age:800
 }
 //Q.1 to count user apperance on web page
+/* 1.Create an express app in node which should count the number of times a user visits a web page
+ and display it to the user.*/
 let count=0;
 app.get('/cookie',(req,res)=>
 {
@@ -56,6 +59,9 @@ app.get('/cookie',(req,res)=>
 })
 
 //Q.2 wheather user present in page or not
+/* 2.Create a route in express which should have a middleware to authenticate a user by checking whether 
+a cookie exists for that particular user or not. If the cookie does not exists then use error handler 
+middleware to throw error message.*/
 let userd={
     name: 'deepakkumar',
     age:800
@@ -87,6 +93,8 @@ app.get("/users",authentication,(req,res)=>
 })
 
 //Q.5 to get from db and show in ejs file
+/* 5.Create a route in express which should accept an object id from the url and if that object id exists in the database 
+then fetch the document of that particular object id and pass it on to the ejs template engine to view the data */
 app.set('view engine', 'ejs');
 app.set('views', __dirname );
 app.get('/fetch/:id',(req,res)=>{
@@ -101,6 +109,7 @@ app.get('/fetch/:id',(req,res)=>{
     
     adder.find({_id:obj_id}).toArray().then((pot)=>
     {
+        console.log(pot)
         res.render('showdata',{pot:pot})  
     })
     
@@ -110,6 +119,7 @@ app.get('/fetch/:id',(req,res)=>{
 
 })
 // Q.3 to read an html file and display in the page
+/* 3.Using the ‘fs’ module in nodejs, read an html file and display its content on the web page */
 app.get("/demo", (req, res) => {
 
         fs.readFile('demo.html', (err, response) => {
